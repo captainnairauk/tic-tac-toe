1c22be83a2c0ec01f8776c7cbf8cab6760f924c8
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2025-08-31 20:44:43 +0530

    Create GameController object structure

--- script.js
+++ script.js
@@ -1,39 +1,78 @@
-const gameBoard = (() => {
-    let board = ["", "", "", "", "", "", "", "", ""];
+const GameBoard = (() => {
+  let board = ["", "", "", "", "", "", "", "", ""];
 
-    const isValidIndex = (index) => index >=0 && index<9;
+  const isValidIndex = (index) => index >= 0 && index < 9;
 
-    const getBoard = () => [...board];
+  const getBoard = () => [...board];
 
-    const placeMark = (index, mark) => {
-        if(isValidIndex(index) && board[index] === ""){
-            board[index] = mark;
-            return true;
-        }
-        return false;
-    };  
+  const placeMark = (index, mark) => {
+    if (isValidIndex(index) && board[index] === "") {
+      board[index] = mark;
+      return true;
+    }
+    return false;
+  };
 
-    const resetBoard = () =>{
-        board = ["", "", "", "", "", "", "", "", ""];
-    };
+  const resetBoard = () => {
+    board = ["", "", "", "", "", "", "", "", ""];
+  };
 
-    return {
-        getBoard,
-        placeMark,
-        resetBoard,
-    };
-
+  return {
+    getBoard,
+    placeMark,
+    resetBoard,
+  };
 })();
 
+const Player = (name, mark) => {
+  let _name = name;
+  let _mark = mark;
 
-const Player = (name, marker) => {
-    let _name = name;
-    let _mark = mark;
+  const getName = () => _name;
+  const getMark = () => _mark;
 
-    const getName = () => _name;
-    const getMark = () => _mark;
-
-    return {getName, getMark};
+  return { getName, getMark };
 };
 
+const GameController = (() => {
+  let currentPlayer;
+  let gameActive = true;
+  // ... other private variables like win conditions, game state
 
+  const switchTurns = () => {
+    //Logic to switch between players
+    //e.g, if current player is player 1, set to player 2, and vice versa
+  };
+
+  const checkWinCondition = () => {
+    //Logic to check if the player has won
+    // This would likely interact with gameboard object
+  };
+
+  const checkDrawCondition = () => {
+    //Logic to check for a draw
+  };
+
+  const endGame = (message) => {
+    gameActive = false;
+    //Display win/draw message
+  };
+
+  const handlePlayerMove = (index) => {
+    if(!gameActive) return;
+    //Call GameBoard's method to place mark
+    //Check for win/draw after move
+    //If no win/draw, switch turns
+  };
+
+  const initializeGame = (player1, player2) =>{
+    //Set initial player, reset game board, set gameActive to true
+  };
+
+  return {
+    initialize: initializeGame,
+    makeMove: handlePlayerMove,
+    //Expose other necessary public methods
+  };
+  
+})();
